c1d284958e2e90bc64ab506e15bbcec6f7fb3ce8
John Vivian <jtvivian@gmail.com>
2019-01-10 13:51:52 -0800

    Add depedencies and setup main

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(name='Bayesian-Outlier-Model',
       version='1.0a1',
-      description='A hierarchical Bayesian model for identifying outliers',
+      description='A hierarchical Bayesian model for identifying outliers in gene expression data',
       url='https://github.com/jvivian/Bayesian-Outlier-Model',
       author='John Vivian',
       author_email='jtvivian@gmail.com',
       license='MIT',
       package_dir={'': 'src'},
-      packages=find_packages('src'), install_requires=['pymc3', 'pandas', 'numpy'])
+      packages=find_packages('src'), install_requires=['pymc3', 'pandas', 'numpy', 'click', 'numba'])
---
+++ src/main.py
@@ -0,0 +1,30 @@
+import pymc3 as pm
+import pandas as pd
+import numpy as np
+
+import click
+
+
+def cli():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
+"""
+TODOS:
+- Inputs
+    - N-of-1 (sample by genes)
+    - Background datasets (samples by genes)
+    - Background vector (length of above)
+- Outputs
+    - 
+- Determine arguments/options
+    - http://click.palletsprojects.com/en/7.x/
+"""
